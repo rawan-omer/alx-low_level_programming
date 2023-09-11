af5f546ea7299219485f59281b8cdbfe1156eb47
rawan-omer <rawanomer0119@gmail.com>
2023-09-11 16:06:15 -0700

    nana

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discreption: A C program that prits numbers.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int r, s;
+
+	for (r = '0'; r < 9; r++)
+	{
+	for (s = r + 1; s < 10; s++)
+	{
+	if (r != s)
+	{
+	putchar(r);
+	putchar(s);
+	if (r == 8 && s ==9)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+	
