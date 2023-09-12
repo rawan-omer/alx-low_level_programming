3078e33ee14f56d76ada2bed6bd6bab8110c252e
rawan-omer <rawanomer0119@gmail.com>
2023-09-12 15:21:26 -0700

    nana

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discreption: A C program that prits with putchar function.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
