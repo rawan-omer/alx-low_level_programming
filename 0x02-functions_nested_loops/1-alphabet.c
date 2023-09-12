d13070c9532436965b5300a9ef64e21e3eeb8064
rawan-omer <rawanomer0119@gmail.com>
2023-09-12 16:51:50 -0700

    nana

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,5 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Discreption: A C program that prits with putchar prototype.
- *
- * Return: Always 0 (Success)
- *
  * print_alphabet - utilizes on the _putchar function to print alphabet a - z
  *
 */
