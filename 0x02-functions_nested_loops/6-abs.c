1dd4c68a0215c80e58bd15409829c4849d9185c6
rawan-omer <rawanomer0119@gmail.com>
2023-09-12 18:36:40 -0700

    nana

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - function that print the absolute value
+ *
+ * @n: takes an integer type input for function
+ *
+ * Return: Always 0 (success)
+*/
+int _abs(int)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
