c717f27d54090e9149f8c5c6b6e2b88fe0a8c0b3
rawan-omer <rawanomer0119@gmail.com>
2023-09-12 17:41:26 -0700

    nana

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _islower - function to check if character is lowercase
+ *
+ * @c: checkes input of function
+ *
+ * Return: returns 1 if 'c' lowercase otherwise always 0 (success)
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
