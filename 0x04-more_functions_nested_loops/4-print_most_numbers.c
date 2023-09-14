639b3dd70d1c68d0b5dd619f0dbf58cfaa23cf37
rawan-omer <rawanomer0119@gmail.com>
2023-09-14 15:55:22 -0700

    nana

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers - a function that prints the numbers, from 0 to 9 exipt 2 and 4
+ * print_most_numbers - function that prints numbers,from 0 to 9 exipt 2 and 4
+ *
+ * Return: Always 0
 */
 
 void print_most_numbers(void)
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,6 +2,8 @@
 
 /**
  * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ *
+ * Return: Always 0
 */
 
 void more_numbers(void)
@@ -13,7 +15,7 @@ void more_numbers(void)
 		for (c = 0; c <= 14; c++)
 		{
 			if (c >= 10 && c <= 14)
-				_putchar(c / 10 + 48);
+				_putchar((c / 10) + 48);
 			_putchar((c % 10) + 48);
 		}
 		_putchar('\n');
