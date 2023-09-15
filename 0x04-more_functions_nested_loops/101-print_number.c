63c69fc41da24e064a0bacb087fdef897feeeaa9
rawan-omer <rawanomer0119@gmail.com>
2023-09-15 14:51:04 -0700

    nana

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
-		i = -i;
+	_putchar('-');
+	i = -i;
 	}
 
 	if ((i / 10) > 0)
 	print_number(i / 10);
-	_putchar(n % 10 + '0');
+	_putchar((n % 10 ) + '0');
 
 }
