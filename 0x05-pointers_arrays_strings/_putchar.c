a429308b489b80980d7de9032ddb6d615deb1af8
rawan-omer <rawanomer0119@gmail.com>
2023-09-18 16:51:45 -0700

    nana

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,6 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: the character to print
  * Return: on success 1
+ * on error -1 is returned and errno is set appropriately
 */
 
 int _putchar(char c)
