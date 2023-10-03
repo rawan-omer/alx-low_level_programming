03c930446c99f9f85d6857e5d8863268905c9675
rawan-omer <rawanomer0119@gmail.com>
2023-10-03 09:24:44 -0700

    finally

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,14 +16,10 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			while (*argv[i] != 0)
+			if (*argv[i] < '0' || *argv[i] > '9')
 			{
-				if (*argv[i] < '0' || *argv[i] > '9')
-				{
-					printf("Erorr\n");
-					return (1);
-				}
-				argv[i]++;
+				printf("Erorr\n");
+				return (1);
 			}
 			sum += atoi(argv[i]);
 		}
