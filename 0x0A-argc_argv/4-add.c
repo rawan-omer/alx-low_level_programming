4dbbcf3db57721405c1dd0db64fc9c580d8688b4
rawan-omer <rawanomer0119@gmail.com>
2023-10-03 09:08:03 -0700

    finally

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] < 48 || *argv[i] > 57)
+			if (*argv[i] < '0' || *argv[i] > '9')
 			{
 				printf("Erorr\n");
 				return (1);
