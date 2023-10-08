3d4d8052b75068c71cdabe3593a23a53d8addbf9
rawan-omer <rawanomer0119@gmail.com>
2023-10-08 07:32:40 -0700

    laala

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	r = calloc(nmemb * sizeof(size));
+	r = malloc(nmemb * sizeof(size));
 
 	if (r == 0)
 		return (NULL);
