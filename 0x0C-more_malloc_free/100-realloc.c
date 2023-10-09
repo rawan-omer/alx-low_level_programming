e120ace6eed26d874e3ed7fef0dce4248d8adf65
rawan-omer <rawanomer0119@gmail.com>
2023-10-09 09:15:43 -0700

    nothing

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,7 @@
 
 /**
  * *_realloc - function that reallocates a memory block
- * @*ptr: pointer
+ * @ptr: pointer
  * @old_size: the old one
  * @new_size: the new
  * Return: pointer
@@ -18,25 +18,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		r = malloc(new_size);
 		return (r);
 	}
-	else if (new_size == old_size)
+	if (new_size == old_size)
 		return (ptr);
-	else if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else
-	{
-		r = malloc(new_size);
-		if (r == NULL)
-			return (NULL);
-		else
-		{
-			for (i = 0; i < old_size && i < new_size; i++)
-				*((char *)r + i) = *((char *) ptr + i);
-			free(ptr);
-			return (r);
-		}
-	}
+	r = malloc(new_size);
+	if (r == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+		*((char *)r + i) = *((char *) ptr + i);
+	free(ptr);
+	return (r);
 }
 
