5eff164b4cea37c67546eecaa7ff14cec891fcc8
rawan-omer <rawanomer0119@gmail.com>
2023-10-24 11:09:54 -0700

    tasks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *node, *tmp;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (!head || !*head)
 		return (-1);
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node, *new = malloc(sizeof(listint_t));
 	unsigned int i = 0;
 
-	if (!*head || !new)
+	if (!head || !new)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
