fa3fa48e585fb32868c1963cf9d1259fa0800484
rawan-omer <rawanomer0119@gmail.com>
2024-02-21 21:30:38 +0000

    alx-programming

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" island_perimeter definition"""
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                p = p + 4
+                if i > 0 and grid[i-1][j] == 1:
+                     p = p - 2
+                if j > 0 and grid[i][j-1] == 1:
+                    p = p - 2
+    return p
